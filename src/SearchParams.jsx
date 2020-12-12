71469fb433db307cc0a4475337d535a080dcf680
Robin Yoong <pulpandmills@gmail.com>
2020-12-12 20:16:08 +0800

    Add useEffect

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+
+  // useEffect schedules the effect to run after the render happens.
+  // When it runs for the first time, it will schedule useEffect, but will not run it until it is done rendering what is returned by the component. This is because you want to show your users something first before you fetch the data
+  useEffect(() => {
+    // Update/clear the breeds so that it starts afresh
+    setBreeds([]);
+    setBreed('');
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings)
+    }, console.error)
+  }, [animal, setBreed, setBreeds]) // this is where you add in your dependencies
 
   return (
     <div className="search-params">
