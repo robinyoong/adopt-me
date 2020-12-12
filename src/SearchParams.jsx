a2ae4a7a02b469979b08b3c90e947c1adcc3dfab
Robin Yoong <pulpandmills@gmail.com>
2020-12-13 00:17:07 +0800

    Add layout for pets, rewrite Pets component

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
+import Results from './Results';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -7,6 +8,17 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []); // set it to an empty array if nothing comes back
+  }
 
   // useEffect schedules the effect to run after the render happens.
   // When it runs for the first time, it will schedule useEffect, but will not run it until it is done rendering what is returned by the component. This is because you want to show your users something first before you fetch the data
@@ -17,13 +29,18 @@ const SearchParams = () => {
 
     pet.breeds(animal).then(({ breeds: apiBreeds }) => {
       const breedStrings = apiBreeds.map(({ name }) => name);
-      setBreeds(breedStrings)
-    }, console.error)
-  }, [animal, setBreed, setBreeds]) // this is where you add in your dependencies
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // this is where you add in your dependencies
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // Prevents JS from submitting a HTML form
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -37,6 +54,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
